9255f68d3c748b693dfa4f3e25277ffcd0255b96
fumikito <guy@hametuha.com>
2019-01-23 20:01:24 +0900

    Now widgets are translation compatible.

--- includes/menu.php
+++ includes/menu.php
@@ -45,3 +45,55 @@ add_filter( 'wp_setup_nav_menu_item', function( $item ) {
 	}
 	return $item;
 } );
+
+/**
+ * Add widget translation.
+ */
+add_action( 'in_widget_form', function( WP_Widget $widget, &$form, $instance ) {
+	if ( ! isset( $instance['title'] ) ) {
+		return;
+	}
+	foreach ( shouyaku_get_locales() as $code => $label ) {
+		if ( ! $code || $code == shouyaku_original_locale() ) {
+			continue;
+		}
+		$id    = strtolower( 'title_' . $code );
+		$title = isset( $instance[ $id ] ) ? $instance[ $id ] : '';
+		?>
+		<p>
+			<label for="<?php echo $widget->get_field_id( $id ) ?>">
+				<?php echo esc_html( sprintf( __( 'Widget Title in %s', 'shouyaku' ), $label ) ) ?>
+			</label>
+			<input class="widefat" id="<?php echo esc_attr( $widget->get_field_id( $id ) ) ?>" name="<?php echo esc_attr( $widget->get_field_name( $id ) ) ?>" value="<?php echo esc_attr( $title ) ?>" />
+		</p>
+		<?php
+	}
+}, 10, 3 );
+
+/**
+ * Save widget
+ */
+add_filter( 'widget_update_callback', function( $instance, $new_instance, $old_instance ) {
+	foreach ( shouyaku_get_locales() as $code => $label ) {
+		if ( ! $code || $code == shouyaku_original_locale() ) {
+			continue;
+		}
+		$name = strtolower( 'title_' . $code );
+		$instance[ $name ] = isset( $new_instance[ $name ] ) ? $new_instance[ $name ] : '';
+	}
+	return $instance;
+}, 10, 3 );
+
+
+/**
+ * Filter widget title.
+ */
+add_filter( 'widget_title', function( $title, $instance, $id_base  ) {
+	$locale = shouyaku_user_locale();
+	if ( ! $locale || $locale == shouyaku_original_locale() ) {
+		return $title;
+	}
+	$key = strtolower( 'title_' . $locale );
+	return isset( $instance[ $key ] ) ? $instance[ $key ] : $title;
+}, 10, 3 );
+
